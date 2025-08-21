322123b747508c1af764bd973260fdb53c86095e
aokitoshihumi <mspw126315@gmail.com>
2025-08-21 17:04:50 +0900

    changeColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import bgimage from '../public/board.jpg';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   //falseが先手でtrueが後手
+  const id = uuidv4();
   const [turn, setTurn] = useState<boolean>(false);
   //6かけ6の盤面をとりあえず作る。
   //0は何もない、1は設置完了,2は白、3は黒
@@ -17,27 +19,12 @@ function App() {
     ['0', '0', '0', '0', '0', '0'],
   ]);
   //const prevBoard = [...board];
-
+  //2ならホワイト、3ならブラック　完了
   //タスク
   //ターン制（true, false)
   const handleClick = (i: number, j: number) => {
-    //console.log(i, j);
-    //もしfalse(自分ターン)なら、黒設置
-    // const [fruits, setFruits] = useState(['りんご', 'バナナ', 'いちご'])
-
-    // const updateFruits = () => {
-    //     setFruits(
-    //         fruits.map((fruit, index) => (index === 2 ? 'ドリアン' : fruit))
-    //     )
-    // }
-
+    //黒
     if (turn && board[i][j] == '0') {
-      //console.log(i, ':', j);
-      // setBoard((prevBoard) => [...prevBoard, [(prevBoard[i][j] = '3')]]);
-      // setBoard([...board, [(prevBoard[i][j] = '3')]]);
-      // setBoard([...prevBoard, prevBoard])
-      // setBoard(board[i][j] = "3")
-      // setBoard(prevBoard);
       setBoard((prevBoard) =>
         prevBoard.map((item: any, inner: number) =>
           item.map((items: any, outer: number) =>
@@ -48,12 +35,7 @@ function App() {
       setTurn(!turn);
       setColor('white');
     } else if (!turn && board[i][j] == '0') {
-      //もしtrue(相手ターン)なら、白設置
-      // prevBoard[i][j] == '2';
-      //console.log(i, ':', j);
-      // setBoard([...board, [(prevBoard[i][j] = '2')]]); //スプレッド構文内の構文について、カッコが何個あればいいのかわからない8/20
-      // setBoard((prevBoard) => [...prevBoard, [(prevBoard[i][j] = '2')]]);
-      // setBoard(prevBoard);
+      //白
       setBoard((prevBoard) =>
         prevBoard.map((item: any, inner: number) =>
           item.map((items: any, outer: number) =>
@@ -68,21 +50,29 @@ function App() {
   //盤面の評価、1は選択可能、2は白、3は黒
   //まぁとりあえず、
   //異なる色に挟まれたら反転する
-  //
+
   return (
     <>
       <div className='boardindex'>
+        {/* おそらく親のidの中の子のidという形で識別をしてくれているのでは */}
         {board.map((item: any, i: number) => {
           return (
-            <div className='boardstyle'>
+            <div className='boardstyle' key={i}>
               {item.map((items: any, j: number) => {
                 return (
+                  //2のボードだけ白、３のボードだけ黒
                   <button
-                    className={
-                      'boardstyles' +
-                      (color == 'white' ? 'whitestyles' : 'blackstyles')
-                    }
                     onClick={() => handleClick(i, j)}
+                    key={j}
+                    className={(() => {
+                      if (board[i][j] == '2') {
+                        return 'whitestyles';
+                      } else if (board[i][j] == '3') {
+                        return 'blackstyles';
+                      } else {
+                        return 'boardstyles';
+                      }
+                    })()}
                   >
                     {board[i][j]}
                   </button>
