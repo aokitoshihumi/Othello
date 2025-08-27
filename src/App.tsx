5e84a437e5d0e8b59f580d3bb7907f91f427aef8
aokitoshihumi <mspw126315@gmail.com>
2025-08-27 14:13:40 +0900

    allowReverse

--- src/App.tsx
+++ src/App.tsx
@@ -35,49 +35,56 @@ function App() {
             //左上
             prevBoard[inner - 1][outer - 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner - 1][outer] === '0' &&
             board[inner - 1][outer] !== undefined
           ) {
             //上
             prevBoard[inner - 1][outer] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner - 1][outer + 1] === '0' &&
             board[inner - 1][outer + 1] !== undefined
           ) {
             //右上
             prevBoard[inner - 1][outer + 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner][outer - 1] === '0' &&
             board[inner][outer - 1] !== undefined
           ) {
             //左
             prevBoard[inner][outer - 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner][outer + 1] === '0' &&
             board[inner][outer + 1] !== undefined
           ) {
             //右
             prevBoard[inner][outer + 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner + 1][outer - 1] === '0' &&
             board[inner + 1][outer - 1] !== undefined
           ) {
             //左下
             prevBoard[inner + 1][outer - 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner + 1][outer] === '0' &&
             board[inner + 1][outer] !== undefined
           ) {
             //下
             prevBoard[inner + 1][outer] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner + 1][outer + 1] === '0' &&
             board[inner + 1][outer + 1] !== undefined
           ) {
@@ -88,6 +95,181 @@ function App() {
         }
       }
     }
+  };
+
+  //black, Whiteの関数内に、挟むための駒が存在するかを判定させる。
+  //黒のコマを置く場合、黒のコマが発生する前に白があればいい
+  //クリックしたときに反転させるプログラム
+  const BlackAllowReverse = (i: number, j: number) => {
+    //黒の場合 Horizontal
+    //console.log('iは', i);
+    //[0,0,1,3,3,2,3,0,0]
+    //[0,0,1,3,3,2,3,0,0]
+    //左から右
+    console.log(i, j);
+    //3を2にする
+    for (let outer = 0; board[i][outer] !== null; outer++) {
+      if (
+        board[i][j + outer] == '2' ||
+        board[i][j + outer] == '0' ||
+        board[i][j + outer] == undefined
+      ) {
+        break;
+      } else if (
+        board[i][j + outer] == '3' &&
+        board[i][j + outer] !== undefined
+      ) {
+        //for (let k = j; k < j + outer; k++) {
+        prevBoard[i][j + outer] == '2';
+        setBoard(prevBoard);
+        //}
+      }
+    }
+    //[      ⚪︎]
+    //[3,2,3,2,3,3,1,0,0]
+
+    //右から左
+    for (let outer = 0; board[i][outer] !== null; outer++) {
+      if (
+        board[i][j - outer] == '2' ||
+        board[i][j - outer] == '0' ||
+        board[i][j - outer] == undefined
+      ) {
+        break;
+      } else if (
+        board[i][j - outer] == '3' &&
+        board[i][j - outer] !== undefined
+      ) {
+        //for (let k = j; k < j + outer; k++) {
+        prevBoard[i][j - outer] == '2';
+        setBoard(prevBoard);
+        //}
+      }
+    }
+    /*[ i= 1 j =1
+        i= 2 j = 2
+        i= 3 j = 3
+        i= 4 j = 4
+       0,0,0,0,0
+       0,1,0,0,0
+       0,0,2,0,0
+       0,0,0,2,0
+       0,0,0,0,3
+    ] */
+    //左上から右下
+    for (let outer = 0; board[i + outer][j + outer] !== null; outer++) {
+      if (
+        board[i + outer][j + outer] == '3' ||
+        board[i + outer][j + outer] == '0' ||
+        board[i + outer][j + outer] == undefined
+      ) {
+        break;
+      } else if (
+        board[i + outer][j + outer] == '2' &&
+        board[i + outer][j + outer] !== undefined
+      ) {
+        //for (let k = j; k < j + outer; k++) {
+        prevBoard[i + outer][j + outer] == '2';
+        setBoard(prevBoard);
+        //}
+      }
+    }
+    //右下から左上
+    for (let outer = 0; board[i - outer][j - outer] !== null; outer++) {
+      if (
+        board[i - outer][j - outer] == '3' ||
+        board[i - outer][j - outer] == '0' ||
+        board[i - outer][j - outer] == undefined
+      ) {
+        break;
+      } else if (
+        board[i - outer][j - outer] == '2' &&
+        board[i - outer][j - outer] !== undefined
+      ) {
+        //for (let k = j; k < j + outer; k++) {
+        prevBoard[i - outer][j - outer] == '2';
+        setBoard(prevBoard);
+        //}
+      }
+    }
+    /*[ i= 3 j = 1
+        i= 2 j = 2
+        i= 1 j = 3
+        i= 0 j = 4
+       0,0,0,0,3
+       0,0,0,2,0
+       0,0,2,0,0
+       0,1,0,0,0
+       0,0,0,0,0
+    ] */
+    //左下から右上
+    for (let outer = 0; board[i - outer][j + outer] !== null; outer++) {
+      if (
+        board[i - outer][j + outer] == '3' ||
+        board[i - outer][j + outer] == '0' ||
+        board[i - outer][j + outer] == undefined
+      ) {
+        break;
+      } else if (
+        board[i - outer][j + outer] == '2' &&
+        board[i - outer][j + outer] !== undefined
+      ) {
+        //for (let k = j; k < j + outer; k++) {
+        prevBoard[i - outer][j + outer] == '2';
+        setBoard(prevBoard);
+        //}
+      }
+    }
+    /*[ i= 3 j = 1
+        i= 2 j = 2
+        i= 1 j = 3
+        i= 0 j = 4
+       0,0,0,0,3
+       0,0,0,2,0
+       0,0,2,0,0
+       0,1,0,0,0
+       0,0,0,0,0
+    ] */
+    //右上から左下
+    for (let outer = 0; board[i + outer][j - outer] !== null; outer++) {
+      if (
+        board[i + outer][j - outer] == '3' ||
+        board[i + outer][j - outer] == '0' ||
+        board[i + outer][j - outer] == undefined
+      ) {
+        break;
+      } else if (
+        board[i + outer][j - outer] == '2' &&
+        board[i + outer][j - outer] !== undefined
+      ) {
+        //for (let k = j; k < j + outer; k++) {
+        prevBoard[i + outer][j - outer] == '2';
+        setBoard(prevBoard);
+        //}
+      }
+    }
+    // for (let outer = 0; outer < 8; outer++) {
+    //   if (board[i][outer] == '2') {
+    //     for(let inner=0; )
+    //   }
+    // }
+    // for (let inner = 0; inner < 8; inner++) {
+    //   for (let outer = 0; outer < 8; outer++) {
+    //     //左から右
+    //     //board[1][0]
+    //     if(board[inner][outer] == '1'){
+    //       for(let i = 0; i < 8; i ++){
+    //         if(board[inner][i] == '3'){
+    //           //board[1][3]
+    //           for(let j = outer; j < i; j++){
+    //             prevBoard[inner][j] = '2';
+    //             setBoard(prevBoard);
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
   };
 
   const BlackCheckBoard = () => {
@@ -101,49 +283,56 @@ function App() {
             //左上
             prevBoard[inner - 1][outer - 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner - 1][outer] === '0' &&
             board[inner - 1][outer] !== undefined
           ) {
             //上
             prevBoard[inner - 1][outer] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner - 1][outer + 1] === '0' &&
             board[inner - 1][outer + 1] !== undefined
           ) {
             //右上
             prevBoard[inner - 1][outer + 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner][outer - 1] === '0' &&
             board[inner][outer - 1] !== undefined
           ) {
             //左
             prevBoard[inner][outer - 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner][outer + 1] === '0' &&
             board[inner][outer + 1] !== undefined
           ) {
             //右
             prevBoard[inner][outer + 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner + 1][outer - 1] === '0' &&
             board[inner + 1][outer - 1] !== undefined
           ) {
             //左下
             prevBoard[inner + 1][outer - 1] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner + 1][outer] === '0' &&
             board[inner + 1][outer] !== undefined
           ) {
             //下
             prevBoard[inner + 1][outer] = '1';
             setBoard(prevBoard);
-          } else if (
+          }
+          if (
             board[inner + 1][outer + 1] === '0' &&
             board[inner + 1][outer + 1] !== undefined
           ) {
@@ -164,6 +353,7 @@ function App() {
           if (inner == i && outer == j) {
             prevBoard[inner][outer] = '2';
             setBoard(prevBoard);
+            BlackAllowReverse(i, j);
           }
         }
       }
